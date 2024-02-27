92e4c526d42f813afd9647e21fe6ce0fcf19b500
Евгения Додиван <evgenita.d97@gmail.com>
2024-02-27 13:49:52 +0100

    im.gal

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,8 +1,22 @@
-<ul>
-	{/* Набір елементів списку із зображеннями */}
-	<li>
-		<div>
-		  <img src="" alt="" />
-		</div>
-	</li>
-</ul>
+import React from "react";
+import css from "./ImageGallery.css";
+import { ImageCard } from "../ImageCard/ImageCard";
+
+export const ImageGallery = ({ photos, openModal }) => {
+  return (
+    <ul className={css.imageList}>
+      {photos.map(({ id, urls: { small, regular }, alt_description }) => (
+        <li
+          className={css.container}
+          key={id}
+          onClick={() => openModal({ src: regular, alt: alt_description })}
+        >
+          <ImageCard
+            small={small}
+            descr={alt_description}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+};
